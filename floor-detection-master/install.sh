4ef357dcf0246d2a9033f31807afc245371ac194
ponjose004 <93509647+ponjose004@users.noreply.github.com>
2024-02-12 11:18:34 +0530

    Delete floor-detection-master directory

--- floor-detection-master/install.sh
+++
@@ -1,7 +0,0 @@
-#install python packages
-python3.7 -m pip uninstall -r ./scripts/old_versions.txt
-python3.7 -m pip install -r ./scripts/requirements.txt
-
-#install mrcnn
-sudo chmod +x ./scripts/install_mrcnn.sh
-sh ./scripts/install_mrcnn.sh
--- floor-detection-master/run.sh
+++
@@ -1,3 +0,0 @@
-
-#change -im_path file to your file directory
-python detect.py --im_path=./teste/095.jpg 
